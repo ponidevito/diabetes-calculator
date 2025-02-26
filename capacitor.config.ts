1bcb6f4d61df7be3492dba2f064690a9bc39add0
unknown <olegmama@gmail.com>
2025-02-26 08:20:19 +0200

    change logo

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.diacalc.app',
+  appName: 'diacalculator',
+  webDir: 'build'
+};
+
+export default config;
